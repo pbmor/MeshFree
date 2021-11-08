f56c9a7a99e891c8fbfbaa1fcf603c936b4797e3
pbmor <peter.mortensen@glasgow.ac.uk>
2021-11-08 13:14:57 +0000

    Update to MeshFree script

--- MeshFree3D.py
+++ MeshFree3D.py
@@ -2,6 +2,9 @@ import numpy as np
 import os
 import sys
 import math
+import csv
+import glob
+import nibabel as nib
 from vtk.util.numpy_support import vtk_to_numpy
 from vtk.numpy_interface import dataset_adapter as dsa
 import vtk
@@ -10,44 +13,84 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 from math import pi
 import warnings
+#from paraview.simple import Contour
 
 class BoxSearch:
     def __init__(self,Points,dx,dy=None,dz=None):
+
+        #Define box resolution if not specified
         if dy is None:
             dy,dz = dx, dx
+
+        #Save resolutions to class
         self.dx, self.dy, self.dz = dx,dy,dz
-        self._minx,self._maxx,self._miny,self._maxy,self._minz,self._maxz = np.min(Points[:,0]),np.max(Points[:,0]),np.min(Points[:,1]),np.max(Points[:,1]),np.min(Points[:,2]),np.max(Points[:,2])
+        
+        #Find maximum and minimum locations in each direction and save to class
+        self._Minx,self._Maxx,self._Miny,self._Maxy,self._Minz,self._Maxz = np.min(Points[:,0]),np.max(Points[:,0]),np.min(Points[:,1]),np.max(Points[:,1]),np.min(Points[:,2]),np.max(Points[:,2])
 
-        self._minx -= dx
-        self._miny -= dy
-        self._minz -= dz
-        self._maxx += dx
-        self._maxy += dy
-        self._maxz += dz
+        #Find and save the centres in each direction
+        self._centrex, self._centrey, self._centrez = ((self._Maxx+self._Minx)/2),((self._Maxy+self._Miny)/2),((self._Maxz+self._Minz)/2)
 
+        #Find the number of boxes from the centre that are required to cover the point space
+        Ndx = np.ceil(self._Maxx-self._centrex)
+        Ndy = np.ceil(self._Maxy-self._centrey)
+        Ndz = np.ceil(self._Maxz-self._centrez)
+
+        #Redefine the maximums and minimums to be defined by the box resolution such that tgere is an integer number of boxes in each direction and there is an extra box beyond the last box containing the points
+        self._minx = self._centrex - (Ndx+1)*dx
+        self._miny = self._centrey - (Ndy+1)*dy
+        self._minz = self._centrez - (Ndz+1)*dz
+        self._maxx = self._centrex + (Ndx+1)*dx
+        self._maxy = self._centrey + (Ndy+1)*dy 
+        self._maxz = self._centrez + (Ndz+1)*dz 
+
+        #Find and save the number of boxes in each direction
         self._nx,self._ny,self._nz = int((self._maxx-self._minx)/self.dx),int((self._maxy-self._miny)/self.dy),int((self._maxz-self._minz)/self.dz)
-        self._N = (self._nx+2)*(self._ny+2)*(self._nz+2)
-        #self._N = (self._nx)*(self._ny)*(self._nz)
+        
+        #Find and save the total number of boxes
+        self._N = (self._nx)*(self._ny)*(self._nz)
+        
+        #Create empty arrays for the the points and respective ids
         self._Box = [[] for i in range(self._N)]
         self._PtIDs = [[] for i in range(self._N)]
         
-        nPts = len(Points)
+        #Save the number of points
+        self.nPts = len(Points)
         
-        for i in range(nPts):
+        #Find box id for each point using BoxID function and save the pointds and arrays to the assigned box array
+        for i in range(self.nPts):
             bid = self.BoxID(Points[i])
             self._Box[bid].append(Points[i])
             self._PtIDs[bid].append(i)
 
     def BoxID(self,X,dix=0,diy=0,diz=0):
-        ix,iy,iz = (int((X[0]-self._minx)/self.dx)+1),(int((X[1]-self._miny)/self.dy)+1),(int((X[2]-self._minz)/self.dz)+1)
-        #ix,iy,iz = (int((X[0]-self._minx)/self.dx)),(int((X[1]-self._miny)/self.dy)),(int((X[2]-self._minz)/self.dz))
+        '''
+        A function to find the box id for a given point
+        Input:
+            - The point of interest.
+
+        Output:
+            - The respective box id
+        '''
+        #Find the location of the point in the box relative to the box resolution
+        ix,iy,iz = (int((X[0]-self._minx)/self.dx)),(int((X[1]-self._miny)/self.dy)),(int((X[2]-self._minz)/self.dz))
         ix += dix
         iy += diy
         iz += diz
 
+        #Return the appropriate box id noting the boxes are listed consecutively
         return ix*self._ny*self._nz + iy*self._nz + iz
 
+    
     def neighb(self,x):
+        '''
+        A function to find the point ids of all the points in the box and surrounding boxes of a specific point.
+        Input:
+            - The point of interest.
+
+        Output:
+            - A list of the point ids
+        '''
         if x[0]<self._minx or x[0]>self._maxx or x[1]<self._miny or x[1]>self._maxy or x[2]<self._minz or x[2]>self._maxz:
             warnings.warn("Point x is out of range of the box")
             return []
@@ -58,80 +101,13 @@ class BoxSearch:
                 for diz in [-1,0,1]:
                     bid = self.BoxID(x,dix,diy,diz)
                     ids.extend(self._PtIDs[bid])
-
+        
         return ids
 
 
-def FindD(Pts,Ranges,r,boxes):
+def ModRK3D(node,nodes,r,supp,supphat,order,boxes,i):
     '''
-    A function to find the standard distance between node points in the three 
-    principle directions.
-
-    Inputs: Pts - Array of the nodes of size N x 3, where N is the number of nodes
-            Ranges - the maximums and minimums in the 3 directions, as a 1 x 6 array,
-                    where the format is [x_min, x_max, y_min, y_max, z_min, z_max]
-            r - box size
-
-    Outputs: Dx, Dy, and Dz -  arrays of the distance to the nearest point of each 
-            point, for the 3 directions. All of size Nx1.
-
-    This function relies on the Pt_Ids function (which assigns each point to a 'box' 
-    in the mesh region) and the Local_Pts (which finds the points in its own and 
-    the surrounding boxes).
-
-    Relies on box size estimation
-    '''
-    # Number of Points
-    N = len(Pts)
-
-    #Find greatest directional difference
-    MeshD = np.zeros(3)
-    for i in range(3):
-        MeshD[i] = Ranges[2*i+1]-Ranges[2*i]
-    MaxD = np.max(MeshD)
-
-    #Create 'empty' arrays filled with overestimations for differences between 
-    #the points and the minimum (non-zero) between 'adjacent' points
-    Diff = (1.1*MaxD)*np.ones((N,N))
-    Dx = (1.1*MaxD)*np.ones(N)
-    Dy = (1.1*MaxD)*np.ones(N)
-    Dz = (1.1*MaxD)*np.ones(N)
-
-    #Get boxes of mesh
-    #b, Mins, bN, bIds = Pt_IDs(Pts,Ranges,r)
-
-    for i in range(N):
-        # Find local points to reduce
-        PointIds =  boxes.neighb(Pts[i,:])
-        #Points, PointIds = Local_Pts(Pts,b,bIds,Mins,bN,i,r)
-        # Find directional distances between each point with their 'local points' 
-        for j in PointIds:
-            Diff[i,j] = np.sqrt(sum(((Pts[i,k]-Pts[j,k])**2 for k in range(3))))
-        
-        # Get the 27 closest points and respective ids (with itself excluded)
-        ClosestIds = np.argpartition(Diff[i,:],27)[:27]
-        Closest = Diff[i,np.argpartition(Diff[i,:],27)[:27]]
-        #Find minimum non-zero distance in each direction, when the point is 
-        #approxiamtely aligned with orthogonally adjacent points, to 4 dp
-        #For example the closest point in the x direction when the y and z are 
-        #approximately equal
-        for j in ClosestIds:
-            if (Pts[i,1]-Pts[j,1]<0.4) and (Pts[i,2]-Pts[j,2]<0.4) and not (i==j):
-                #overwrite the existing 'closest point' distance'
-                if (Dx[i]>abs(Pts[i,0]-Pts[j,0])) and (abs(Pts[i,0]-Pts[j,0])>0.2):
-                    Dx[i] = round(abs(Pts[i,0]-Pts[j,0]),4)
-            if (Pts[i,0]-Pts[j,0]<0.4) and (Pts[i,2]-Pts[j,2]<0.4) and not (i==j):
-                if (Dy[i]>abs(Pts[i,0]-Pts[j,1])) and (abs(Pts[i,1]-Pts[j,1])>0.2):
-                    Dy[i] = round(abs(Pts[i,1]-Pts[j,1]),4)
-            if (Pts[i,0]-Pts[j,0]<0.4) and (Pts[i,1]-Pts[j,1]<0.4) and not (i==j):
-                if (Dz[i]>abs(Pts[i,2]-Pts[j,2])) and (abs(Pts[i,2]-Pts[j,2])>0.2):
-                    Dz[i] = round(abs(Pts[i,2]-Pts[j,2]),4)
-        
-    return Dx, Dy, Dz
-
-def ModRK3D(node,nodes,r,order,boxes,i):
-    '''
-    A function to find the shape functions and derivatives of the points in a mesh
+    A function to find the shape functions and derivatives of the a specific point
 
     Inputs: nodes - Nodes of the Mesh
             r - radius to define support radii
@@ -139,10 +115,6 @@ def ModRK3D(node,nodes,r,order,boxes,i):
             weight - weighting of nodes (assumed to be constant)
             Ranges - Maximum and Minimum values of the mesh, in the 3 directions
     '''
-    
-    #Define support radii
-    supp = 3*r
-    supphat = 0.9*r
 
     #Get number of nodes
     nNodes = len(nodes)
@@ -179,31 +151,23 @@ def ModRK3D(node,nodes,r,order,boxes,i):
     Pos[17,:]=[-phi,0.,1./phi]
     Pos[18,:]=[phi,0.,-1./phi]
     Pos[19,:]=[-phi,0.,-1./phi]
-
-    Pos = np.zeros((6,3))
-    Pos[0,:]=[0.,0.,1.]
-    Pos[1,:]=[0.,0.,-1.]
-    Pos[2,:]=[1.,0.,0.]
-    Pos[3,:]=[-1.,0.,0.]
-    Pos[4,:]=[0.,1.,0.]
-    Pos[5,:]=[0.,-1.,0.]
     
+    #Normalise the dodecahedron locations
     for j in range(len(Pos)):
         Pos[j,:] = (Pos[j,:]/np.sqrt(sum((Pos[j,k]**2 for k in range(3)))))
-
-    #for i in range(nNodes):        
+    
     print('Finding shape derivatives of node: ',i,' ',node)
 
     #Get the shape function of the point
     shape_pt = ModRK3DShape(node,nodes,supp,supphat,order,boxes)
-        
+    
+    #Get shape functions of the points of the dodecahdron surrounding the point of interest
     for j in range(len(Pos)):
         X = (node+(Pos[j,:])*r)
         shape = ModRK3DShape(X,nodes,supp,supphat,order,boxes)
     
         #Sum shape points for each dodecahedron point to find finite 
         #difference definition of derivatives
-        
         shape_dx += shape*Pos[j,0]
         shape_dy += shape*Pos[j,1]
         shape_dz += shape*Pos[j,2]
@@ -214,7 +178,8 @@ def ModRK3D(node,nodes,r,order,boxes,i):
     shape_dy *= 3/(r*len(Pos))
     shape_dz *= 3/(r*len(Pos))
     
-    return Shape, shape_dx, shape_dy,shape_dz
+    return Shape, shape_dx, shape_dy, shape_dz
+
 
 def ModRK3DShape(node,nodes,supp,supphat,order,boxes):
     '''
@@ -262,7 +227,6 @@ def ModRK3DShape(node,nodes,supp,supphat,order,boxes):
         Fhat += HT*phihat
  
     #Get inverse of M
-
     invM = np.linalg.inv(M)
     
     for j in LP_Ids:
@@ -272,7 +236,6 @@ def ModRK3DShape(node,nodes,supp,supphat,order,boxes):
         Diff = node-s
         #Get difference magnitude
         DiffMag = np.sqrt(sum((Diff[k]**2 for k in range(3))))
-        #if not DiffMag ==0:
         #Define phi and phi_hat
         phi=calc_phi(DiffMag,supp)
         phihat=calc_phi(DiffMag,supphat)
@@ -283,6 +246,7 @@ def ModRK3DShape(node,nodes,supp,supphat,order,boxes):
         shape[j] = H.dot(invM.dot((HT0-Fhat)))*phi+phihat
     
     return shape
+
 
 def basis(x,order):
     '''
@@ -303,6 +267,7 @@ def basis(x,order):
     HT = H.transpose()
     return H, dH, HT
 
+
 def calc_phi(z,a):
     '''
     Calculate phi
@@ -321,72 +286,135 @@ def calc_phi(z,a):
 
 
 if __name__=='__main__':
-        
-    #Get points
-    Fname = '../RunVTK/Point_Clouds/bav02/propagated point clouds/seg05_to_01_bav02_root_pointcloud.vtk'
- 
-    # Read the source file.
-    reader = vtk.vtkPolyDataReader()
-    reader.SetFileName(Fname)
+    
+    '''
+    nii = nib.load(Fname)
+    nii_header = nii.header
+    pixdim = nii.header['pixdim']
+    Vol = np.prod(pixdim[1:4])
+
+    DMax = np.max(pixdim[1:4])
+    DMin = np.min(pixdim[1:4])
+
+    
+    os.system('/Applications/ITK-SNAP.app/Contents/bin/c3d ' + Fname + ' -trim 5vox -o temp_vtk.vtk')
+
+    reader = vtk.vtkGenericDataObjectReader()
+    reader.SetFileName(ref)
     reader.ReadAllScalarsOn()
-    reader.ReadAllVectorsOn()
     reader.Update()
-    
-    ######################################
-    # Define File Data
-    polydata  = reader.GetOutput()
-    Pts = vtk_to_numpy(polydata.GetPoints().GetData())
-    Ranges = np.array(polydata.GetPoints().GetBounds())
-    
-    #Find Dx, Dy, Dz with local point search radius of 1.5
-    #boxes = BoxSearch(Pts,1.5)
-    #Dx, Dy, Dz = FindD(Pts,Ranges,1.5,boxes)
+    new_data = reader.GetOutput()
+
+    thresh = vtk.vtkThreshold()
+    thresh.SetInputData(new_data)
+    thresh.Scalars = ['POINTS', 'scalars']
+    thresh.ThresholdRange = [0.5, 5.0]
+    thresh.Update()
     '''
-    x = np.linspace(0, 10,6)#[0.0,1.0]
-    y = np.linspace(0, 10,6)
-    z = np.linspace(0, 10,6)
-    Pts = np.zeros((216,3))
 
-    for i in range(len(x)):
-        for j in range(len(y)):
-            for k in range(len(z)):
-                Pts[k+6*j+36*i,:] = [x[i], y[j], z[k]]
-    '''
+    Fname = 'Point_Clouds/bav01/seg05_to_02_bav01_root_reslice.nii.gz'
+
+    DMax = 0.55
+    DMin = 0.48
+
+    List_of_Subdirectories = sorted(glob.glob('./PropagatedPointClouds/*'))
+    CommonOfDir = os.path.commonprefix(List_of_Subdirectories)
+    for d in List_of_Subdirectories:
+        DataDir = d.replace(CommonOfDir,'')
+        
+        with open('../RunVTK/echoframetime.csv') as csv_file:
+            XLData = csv.reader(csv_file, delimiter=',')
+            for row in XLData:
+                if DataDir == row[0]:
+                    DataInfo = row
+
+        #Define Frame Time Length
+        FT = DataInfo[1]
+        #Define Open Frame and Close Frame
+        OF = DataInfo[4]
+        CF = DataInfo[5]
+
+        #for 'medial_meshes' in List_of_subdirectories:
+        fnames = sorted(glob.glob(d + '/propagated point clouds/*.vtk'))
+
+        #Choose reference frame
+        refN = int(DataInfo[4])-1 #Choose frame before valve opens
+
+        common = os.path.commonprefix(fnames)
+        for Fname in list(fnames):
+            X = Fname.replace(common,'')
+            X = X.replace('.vtk','')
+            X = np.fromstring(X, dtype=int, sep=' ')
+            X=X[0]
+            if X==refN:
+                ref=Fname
+        NX = len(fnames)
+
+        if not fnames:
+            print(DataDir," is empty")
+        else:
+            fdir = os.path.dirname(fnames[0])
+            # Check Directory
+            if not os.path.exists(fdir):
+                print('Error: Path does not exist:', fdir)
+                sys.exit()
+            if DataInfo[9] == 'n':
+                print(DataDir,' is excluded')
+            else:
+                print(DataDir,' is included')
+
+                reader = vtk.vtkPolyDataReader()
+                reader.SetFileName(ref)
+                reader.ReadAllScalarsOn()
+                reader.Update()
+                polydata = reader.GetOutput()
+                Pts_ref = vtk_to_numpy(polydata.GetPoints().GetData())
+                #print(dir(polydata))
+                #Spacing = polydata.GetSpacing()
+                
+                Vol  = DMin**3
+                r    = (Vol*(3/(4*math.pi)))**(1/3)
+                supp = 3.0*DMax
+                supphat = 0.9*DMin
     
-
-    #print(Dx, Dy, Dz)
-    #print(np.max([Dx,Dy,Dz]))
-    #print(np.mean([Dx,Dy,Dz]))
-    r = 30.1#5.1#*0.7777777#*np.max([Dx,Dy,Dz])
-
-    #print (Ranges)
-    boxes = BoxSearch(Pts,r)
-
-    #Create empty arrays
-    nNodes = len(Pts[:,0]) 
-    nDOF = nNodes
-    PtDiff = np.zeros(nNodes-1)
-
-    # Order of the basis
-    order = 1 
-
-    #print('Find Shape function and its derivatives...')
-    #shape, shape_dx, shape_dy, shape_dz = ModRK3D(Pts,r,order,boxes)
-    #print('...Done')
+                print('DMax =',DMax)
+                print('DMin =',DMin)
+                print('Finding boxes...')
+                boxes = BoxSearch(Pts_ref,r+supp)
+                print('Found')
     
-    #Create empyt array for deformation gradient
-    F = np.zeros((3,3))
-    
-    for i in range(nNodes):
-        F = np.zeros((3,3))
-        LP_Ids = boxes.neighb(Pts[i])
-        shape, shape_dx, shape_dy, shape_dz = ModRK3D(Pts[i,:],Pts,r,order,boxes,i)
-        for j in LP_Ids:
-            X = Pts[j,:]
-            F[0,:] += shape_dx[j]*X
-            F[1,:] += shape_dy[j]*X
-            F[2,:] += shape_dz[j]*X
-        print('Point_',i,':')
-        print('F=',F)
-    
+                #Create empty arrays
+                nNodes = len(Pts_ref[:,0])
+                nDOF = nNodes
+                PtDiff = np.zeros(nNodes-1)
+
+                # Order of the basis
+                order = 1 
+
+                #Cycle through frames
+                for Fname in ref: #fnames:
+                    reader = vtk.vtkPolyDataReader()
+                    reader.SetFileName(ref)
+                    reader.ReadAllScalarsOn()
+                    reader.Update()
+                    polydata = reader.GetOutput()  
+                    Pts = vtk_to_numpy(polydata.GetPoints().GetData())
+        
+                    #Create empty array for deformation gradient
+                    F = np.zeros((3,3))
+        
+                    for i in range(nNodes):
+                        F = np.zeros((3,3))
+                        #Get List of local point ids
+                        LP_Ids = boxes.neighb(Pts[i])
+                        #Get shape functions
+                        shape, shape_dx, shape_dy, shape_dz = ModRK3D(Pts_ref[i,:],Pts_ref,r,supp,supphat,order,boxes,i)
+                        for j in LP_Ids: 
+                            X = Pts[j,:]
+                            F[:,0] += shape_dx[j]*X
+                            F[:,1] += shape_dy[j]*X
+                            F[:,2] += shape_dz[j]*X
+            
+                        print('Point_',i,':')
+                        print('F=',F)
     
